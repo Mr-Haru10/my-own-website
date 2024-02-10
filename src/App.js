c2dad98cfd16be5877b8edc8db59e324d28e1405
Mr-Haru10 <harshamundaragi555@gmail.com>
2024-02-10 21:13:19 +0530

    Added nav bar with name and added animation to image and nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavBar from './Components/NavBar';
 import TopImage from './Components/TopImage';
 
 function App(){
     return (
         <div className='appDiv'>
+            <NavBar />
             <TopImage />
         </div>
     );
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <div className="navBarDiv">
+        <h1>Harsha R Mindaragi</h1>
+
+        </div>
+    );
+}
+
+export default NavBar;
